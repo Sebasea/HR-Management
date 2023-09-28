7c97688e9284abdab915816bc05d714f094b36e4
Sebasea <js.virac2905@gmail.com>
2023-09-27 22:30:41 -0500

    Crea reporte jacoco

--- hrmanament/src/main/java/com/hrManagement/HrmanamentApplication.java
+++ hrmanament/src/main/java/com/hrManagement/HrmanamentApplication.java
@@ -1,8 +1,10 @@
 package com.hrManagement;
 
+import lombok.Generated;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+@Generated
 @SpringBootApplication
 public class HrmanamentApplication {
 
